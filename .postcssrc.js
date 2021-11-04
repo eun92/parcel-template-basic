beb02a46ee6b8199ea1154b9f87efe1c77009ece
eun <qoo0131@gmail.com>
2021-11-04 16:52:03 +0900

    create project

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+// ESM - import / export
+// CommonJS (node js) - require / module.exports
+
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require("autoprefixer")
+
+// export {
+// plugins : [
+//     autoprefixer
+//   ]
+// }
+
+module.exports = {
+  plugins: [require("autoprefixer")],
+}
